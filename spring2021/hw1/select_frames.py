30a95ddaa5980995a14ab7d6ca8595179ad5f35a
JunweiLiang <junweil@cs.cmu.edu>
2021-02-20 13:46:01 -0500

    Updated code for spring2021

--- spring2021/hw1/select_frames.py
+++ spring2021/hw1/select_frames.py
@@ -1,41 +1,39 @@
 #!/bin/python
-# Randomly select 
+# Randomly select
 
+import argparse
 import numpy
 import os
 import sys
 
-if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print("Usage: {0} file_list select_ratio output_file".format(sys.argv[0]))
-        print("file_list -- the list of video names")
-        print("select_ratio -- the ratio of frames to be randomly selected from each audio file")
-        print("output_file -- path to save the selected frames (feature vectors)")
-        exit(1)
+parser = argparse.ArgumentParser()
+parser.add_argument("file_list")
+parser.add_argument("select_ratio", type=float)
+parser.add_argument("output_file")
+parser.add_argument("--mfcc_path", default="mfcc")
 
-    file_list = sys.argv[1]; output_file = sys.argv[3]
-    ratio = float(sys.argv[2])
+if __name__ == "__main__":
+  args = parser.parse_args()
 
-    fread = open(file_list,"r")
-    fwrite = open(output_file,"w")
+  fread = open(args.file_list, "r")
+  fwrite = open(args.output_file, "w")
 
-    # random selection is done by randomizing the rows of the whole matrix, and then selecting the first 
-    # num_of_frame * ratio rows
-    numpy.random.seed(18877)
+  # random selection is done by randomizing the rows of the whole matrix, and then selecting the first
+  # num_of_frame * ratio rows
+  numpy.random.seed(18877)
 
-    for line in fread.readlines():
-        mfcc_path = "mfcc/" + line.replace('\n','') + ".mfcc.csv"
-        if os.path.exists(mfcc_path) == False:
-            continue
-        array = numpy.genfromtxt(mfcc_path, delimiter=";")
-        numpy.random.shuffle(array)
-        select_size = int(array.shape[0] * ratio)
-        feat_dim = array.shape[1]
+  for line in fread.readlines()[1:]:  # skipped the header
+    mfcc_path = os.path.join(args.mfcc_path, line.strip().split(",")[0] + ".mfcc.csv")
+    if not os.path.exists(mfcc_path):
+      continue
+    array = numpy.genfromtxt(mfcc_path, delimiter=";")
+    numpy.random.shuffle(array)
+    select_size = int(array.shape[0] * args.ratio)
+    feat_dim = array.shape[1]
 
-        for n in xrange(select_size):
-            line = str(array[n][0])
-            for m in range(1, feat_dim):
-                line += ';' + str(array[n][m])
-            fwrite.write(line + '\n')
-    fwrite.close()
-
+    for n in xrange(select_size):
+      line = str(array[n][0])
+      for m in range(1, feat_dim):
+        line += ";" + str(array[n][m])
+      fwrite.write(line + "\n")
+  fwrite.close()
